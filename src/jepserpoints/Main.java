7d14513f9b9c8015118b994d458299b0a8a58966
Jesper Bergström <jesper.b96@gmail.com>
2018-03-26 16:00:29 +0200

    Algorithm mostly done

--- src/jepserpoints/Main.java
+++ src/jepserpoints/Main.java
@@ -8,23 +8,35 @@ import java.util.Collections;
 import java.util.Scanner;
 
 public class Main {
-	
-	private static double od = 0;
 
 	public static void main(String[] args) {
 		File[] files = new File("res").listFiles();
 		for (int file = 0; file < files.length; file += 1) {
 			try {
 				Scanner scan = new Scanner(files[file], "UTF-8");
-				double cs = 0;
 				String str = scan.nextLine();
+				double od = 0;
+				double cs = 0;
+				double ar = 0;
+				boolean hidden = false;
+				boolean flashlight = false;
+				double timeMultiplier = 1;
 				while (!str.contains("[HitObjects]")) {
 					if(str.startsWith("CircleSize:")){
 						cs = Double.parseDouble(str.split(":")[1]);
 					} else if(str.startsWith("OverallDifficulty:")){
 						od = Double.parseDouble(str.split(":")[1]);
+					} else if(str.startsWith("ApproachRate:")){
+						ar = Double.parseDouble(str.split(":")[1]);
 					}
 					str = scan.nextLine();
+				}
+				if(file == files.length - 3){
+					hidden = true;
+					double[] arr = useHardRock(od, cs, ar);
+					od = arr[0];
+					cs = arr[1];
+					System.out.println(od + ", " + cs);
 				}
 
 				ArrayList<Double> allNotes = new ArrayList<Double>();
@@ -59,6 +71,7 @@ public class Main {
 						long nwTime = Long.valueOf(parts[2]);
 						long deltaTime = nwTime - time;
 						time = nwTime;
+						deltaTime /= timeMultiplier;
 
 						// Get spacing between last object and current object.
 						double spacing = 0;
@@ -149,10 +162,10 @@ public class Main {
 					}
 				}
 
-				double p95 = getTotalPPValue(allNotes, 95.0, 0, maxCombo, maxCombo, accCombo);
-				double p98 = getTotalPPValue(allNotes, 98.0, 0, maxCombo, maxCombo, accCombo);
-				double p99 = getTotalPPValue(allNotes, 99.0, 0, maxCombo, maxCombo, accCombo);
-				double p100 = getTotalPPValue(allNotes, 100.0, 0, maxCombo, maxCombo, accCombo);
+				double p95 = getTotalPPValue(allNotes, 95.0, 0, maxCombo, maxCombo, accCombo, od, ar, hidden, flashlight);
+				double p98 = getTotalPPValue(allNotes, 98.0, 0, maxCombo, maxCombo, accCombo, od, ar, hidden, flashlight);
+				double p99 = getTotalPPValue(allNotes, 99.0, 0, maxCombo, maxCombo, accCombo, od, ar, hidden, flashlight);
+				double p100 = getTotalPPValue(allNotes, 100.0, 0, maxCombo, maxCombo, accCombo, od, ar, hidden, flashlight);
 				
 				System.out.println(files[file].getName());
 				System.out.println("95%: " + Math.round(p95) + "  98%: " + Math.round(p98) + "  99%: " + Math.round(p99) + "  100%: " + Math.round(p100));
@@ -164,7 +177,7 @@ public class Main {
 		}
 	}
 
-	private static double getTotalPPValue(ArrayList<Double> list, double acc, int missCount, int maxCombo, int potMaxCombo, int accCombo) {
+	private static double getTotalPPValue(ArrayList<Double> list, double acc, int missCount, int maxCombo, int potMaxCombo, int accCombo, double od, double ar, boolean hidden, boolean flashlight) {
 		double difficulty = 0;
 		Collections.sort(list);
 		for (int i = 0; i < list.size() && i < 60; i++) {
@@ -187,12 +200,72 @@ public class Main {
 			difficulty *= Math.min(Math.pow(potMaxCombo, 0.8) / Math.pow(maxCombo, 0.8), 1.0);
 		}
 		
+		// AR difficulty adjustments
+		double arFactor = 1.0;
+		if(ar > 10.33){
+			arFactor += 0.45 * (ar - 10.33);
+		} else if(ar < 8.0){
+			if(hidden){
+				arFactor += 0.02 * (8.0 - ar);
+			} else {
+				arFactor += 0.01 * (8.0 - ar);
+			}
+		}
+		
+		difficulty *= arFactor;
+		
+		if(hidden){
+			difficulty *= 1.18;
+		}
+		
+		if(flashlight){
+			difficulty *= 1.45 * lengthBonus;
+		}
+		
 		// Calculate accuracy
 		double accValue = Math.pow(1.52163, od) * Math.pow(acc/100, 24) * 2.83;
 		accValue *= Math.min(1.15, Math.pow(accCombo / 1000.0, 0.3));
 		difficulty += accValue;
 		
 		return difficulty;
+	}
+	
+	private static double[] useDoubleTime(double od, double timeMultiplier, double ar){
+		od = msToOd(odToMs(od) * 2 / 3);
+		timeMultiplier = 1.5;
+		ar = msToAr(arToMs(ar) * 2 / 3.0);
+		double[] arr = {od, timeMultiplier, ar};
+		return arr;
+	}
+	
+	private static double[] useHardRock(double od, double cs, double ar){
+		od = Math.min(10,  od * 1.4);
+		cs *= 1.3;
+		ar = Math.min(10, ar * 1.4);
+		double[] arr = {od, cs, ar};
+		return arr;
+	}
+	
+	// Stole this from Tillerino hehehehe xd
+	private static double odToMs(double od) {
+		return 79.5 - Math.ceil(6 * od);
+	}
+	
+	// This as well hehehehe
+	private static double msToOd(double ms) {
+		return (79.5 - ms) / 6;
+	}
+	
+	public static double arToMs(double ar) {
+		if(ar < 5)
+			return 1800 - ar * 120;
+		return 1200 - 150 * (ar - 5);
+	}
+
+	public static double msToAr(double ms) {
+		if(ms > 1200)
+			return (1800 - ms) / 120;	
+		return (1200 - ms) / 150 + 5;
 	}
 
 	private static double getAngle(int x1, int y1, int x2, int y2, int x3, int y3) {
